eb769d5504fd81770d595919fd56ec1bc992029b
brozozowska <s_rozov@me.com>
2024-03-13 17:02:44 +0300

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Сергей Розов on 13.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
