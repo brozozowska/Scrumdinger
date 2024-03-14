09ed5f4bd3a47bddecd9418f9b1885d1d4b606cf
brozozowska <s_rozov@me.com>
2024-03-14 18:13:11 +0300

    Chapter 3. Creating a navigation hierarchy - complete. Move view files to Views group.

---
+++ Scrumdinger/Views/ScrumsView.swift
@@ -0,0 +1,33 @@
+//
+//  ScrumsView.swift
+//  Scrumdinger
+//
+//  Created by Сергей Розов on 14.03.2024.
+//
+
+import SwiftUI
+
+struct ScrumsView: View {
+    let scrums: [DailyScrum]    
+    var body: some View {
+        NavigationStack {
+            List(scrums) { scrum in
+                NavigationLink(destination: DetailView(scrum: scrum)) {
+                    CardView(scrum: scrum)
+                }
+                .listRowBackground(scrum.theme.mainColor)
+            }
+            .navigationTitle("Daily Scrums")
+            .toolbar {
+                Button(action: {}) {
+                    Image(systemName: "plus")
+                }
+                .accessibilityLabel("New Scrum")
+            }
+        }
+    }
+}
+
+#Preview {
+    ScrumsView(scrums: DailyScrum.sampleData)
+}
