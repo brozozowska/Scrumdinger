56dfa0c0c6989270e718e504fa876ddbb3323133
brozozowska <s_rozov@me.com>
2024-03-14 16:48:34 +0300

    Chapter 2. Displaying data in a list - complete.

---
+++ Scrumdinger/ScrumsView.swift
@@ -0,0 +1,24 @@
+//
+//  ScrumsView.swift
+//  Scrumdinger
+//
+//  Created by Сергей Розов on 14.03.2024.
+//
+
+import SwiftUI
+
+struct ScrumsView: View {
+    
+    let scrums: [DailyScrum]
+    
+    var body: some View {
+        List(scrums) { scrum in
+            CardView(scrum: scrum)
+                .listRowBackground(scrum.theme.mainColor)
+        }
+    }
+}
+
+#Preview {
+    ScrumsView(scrums: DailyScrum.sampleData)
+}
